da915eb12a466b24de389b4c608ecd0736a34624
TaNgocHai <ngochai260720@gmail.com>
2023-12-25 18:28:29 +0700

    first commit

---
+++ component/Header.js
@@ -0,0 +1,58 @@
+//header
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const { width } = Dimensions.get('window');
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <Icon name="menu-outline" size={30} color="#000" />
+                </TouchableOpacity>
+                <Text style={styles.title}>Hải bố đời </Text>
+                <TouchableOpacity>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.searchBar}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Tìm kiếm sản phẩm?"
+                />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'blue', // Đổi màu nền thành blue
+        padding: 10,
+        width: width,
+        position: 'absolute',
+        top: 35,
+        zIndex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    searchBar: {
+        marginTop: 10,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 5,
+    },
+    input: {
+        height: 40,
+        paddingLeft: 10,
+    },
+});
+
+export default Header;
